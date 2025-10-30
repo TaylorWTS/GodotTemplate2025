71251cdacd8b92cd3a9f1a804b99580ca00a7767
TaylorWTS <148837328+TaylorWTS@users.noreply.github.com>
2025-10-30 11:29:59 +1100

    fixedGameVer

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,11 +8,10 @@ const JUMP_VELOCITY = -300.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _physics_process(delta):
-	# Handle jump.
-	if Input.is_action_just_pressed("Move_Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+@onready var CoyoteClock = $Timer
+var CoyoteLatch: bool = false
 
+func _physics_process(delta):
 	# Get the Input Direction
 	var direction = Input.get_axis("Move_Left", "Move_Right")
 	
@@ -24,14 +23,21 @@ func _physics_process(delta):
 	
 	# Add the gravity and play animations
 	if not is_on_floor():
+		if CoyoteClock.is_stopped():
+			CoyoteClock.start()
 		velocity.y += gravity * delta
 		animated_sprite.play("Jump")
 	else:
+		CoyoteLatch = false
 		if direction == 0:
 			animated_sprite.play("Idle")
 		else:
 			animated_sprite.play("Walk")
-
+	# Handle jump.
+	if Input.is_action_just_pressed("Move_Jump") and (is_on_floor() or !CoyoteClock.is_stopped()):
+		if CoyoteLatch == false:
+			CoyoteLatch = true
+			velocity.y = JUMP_VELOCITY
 	# Apply Movement
 	if direction:
 		velocity.x = direction * SPEED
